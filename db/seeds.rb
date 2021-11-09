66a6bc7a1e12499daee43374af768c86f0b52a8a
Yann Lochkareff <yann.lochkareff@gmail.com>
2021-11-09 16:58:25 +0100

    Exercice 3 done

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require 'faker'
+
+puts 'Creating 10 fake articles...'
+10.times do
+  article = Article.new(
+    title: Faker::Sports::Basketball.team,
+    content: Faker::Sports::Basketball.player
+  )
+  article.save!
+end
+puts 'Finished!'
